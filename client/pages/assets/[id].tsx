9979b6faf09a944e80bac4a14c90b0568126acab
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-03-23 10:52:25 +0100

    Add content in Homepage

--- client/pages/assets/[id].tsx
+++ client/pages/assets/[id].tsx
@@ -30,7 +30,7 @@ const SingleAsset = () => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Box textAlign='center'>
+            <Box mt='16' textAlign='center'>
                 <Heading size='2xl'>Asset : {id}</Heading>
                 <Text fontSize='xl' mt='10'>Multiply your exposure to your favorite crypto assets. Browse our featured products or select a asset.</Text>
                 <Text fontSize='xl'>We&apos;ve launched Multiply for Aave.</Text>
