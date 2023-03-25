2e670f42908d8a255aa0fde0ef1c7415813fd266
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-03-25 10:34:51 +0100

    Goerli deploy works

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     gasReporter: {
@@ -14,7 +17,15 @@ const config: HardhatUserConfig = {
         localhost: {
             url: "http://127.0.0.1:8545",
             chainId: 31337,
-        }
+        },
+        goerli: {
+            url: `https://goerli.infura.io/v3/${process.env.INFURA_ALYRA_PROJECTS_API_KEY}`,
+            accounts: [process.env.ACCOUNT_1_PRIVATE_KEY || ""],
+        },
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${process.env.INFURA_ALYRA_PROJECTS_API_KEY}`,
+            accounts: [process.env.ACCOUNT_1_PRIVATE_KEY || ""],
+          }
     },
     solidity: "0.8.19",
 };
--- client/contracts/simpleStorage.ts
+++ client/contracts/simpleStorage.ts
@@ -1,4 +1,5 @@
-export const contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+// export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3" // localhost
+export const contractAddress = "0xec6d597f09ee9866c030Ed436E81D929559c3de6" // goerli
 
 export const abi = [
     {
