b06b22ff153961a630d24dfc5b63bf7ae3d2f3aa
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-04-08 09:24:18 +0200

    startSellingSession testing function and endSellingFunction testing function work

--- backend/test/FinancialVehicle.ts
+++ backend/test/FinancialVehicle.ts
@@ -1,4 +1,3 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
@@ -11,10 +10,10 @@ const admins = [
 ];
 
 describe("Financial Vehicle", () => {
-    let FinancialVehicle;
+    let FinancialVehicle: any;
     let financialVehicle: any;
-    let Asset;
-  let asset;
+    let Asset: any;
+    let asset: any;
 
     beforeEach(async function () {
         FinancialVehicle = await ethers.getContractFactory("FinancialVehicle");
@@ -23,9 +22,9 @@ describe("Financial Vehicle", () => {
         expect(financialVehicle).to.exist;
 
         // Déploiement du contrat Asset
-    Asset = await ethers.getContractFactory("Asset");
-    asset = await Asset.deploy();
-    await asset.deployed();
+        Asset = await ethers.getContractFactory("Asset");
+        asset = await Asset.deploy();
+        await asset.deployed();
     });
 
     // beforeEach(async function () {
@@ -81,12 +80,12 @@ describe("Financial Vehicle", () => {
       expect(await assetInstance.symbol()).to.equal("ASSET");
       expect(await assetInstance.totalSupply()).to.equal(1000);
 
-    //   // Vérification que l'Asset a bien été ajouté à la liste des assets
-    //   const assets = await financialVehicle.assets();
-    //   expect(assets[0].assetAddress).to.equal(tokenAddress);
-    //   expect(assets[0].name).to.equal("Asset Name");
-    //   expect(assets[0].symbol).to.equal("ASSET");
-    //   expect(assets[0].totalSupply).to.equal(1000);
+        // Vérification que l'Asset a bien été ajouté à la liste des assets
+        const assets = await financialVehicle.getAssets();
+        expect(assets[0].assetAddress).to.equal(tokenAddress);
+        expect(assets[0].name).to.equal("Asset Name");
+        expect(assets[0].symbol).to.equal("ASSET");
+        expect(assets[0].totalSupply).to.equal(1000);
     });
 
     // it("should only be called by an admin", async function () {
@@ -102,31 +101,69 @@ describe("Financial Vehicle", () => {
     // });
   });
 
+  describe("startSellingSession", () => {
+    it("should emit a SellingStatusChange event", async () => {
+      await expect(financialVehicle.startSellingSession(asset.address))
+        .to.emit(financialVehicle, "SellingStatusChange")
+        .withArgs(asset.address, 1 /* SellingStatus.SellingSessionStarted */);
+    });
 
+    it("should revert if the selling session has already started", async () => {
+      await financialVehicle.startSellingSession(asset.address);
+      await expect(financialVehicle.startSellingSession(asset.address))
+        .to.be.revertedWith("Selling session already started");
+    });
+
+    // it("should revert if the selling session has already ended", async () => {
+    //     // const [otherAccount] = await ethers.getSigners();
+    //     const [owner, addr1, addr2] = await ethers.getSigners();
+    //   await financialVehicle.startSellingSession(asset.address);
+    //   await asset.approve(financialVehicle.address, 1000);
+    //   await financialVehicle.connect(addr1).buyToken(asset.address, 101);
+    //   await financialVehicle.startSellingSession(asset.address);
+    //   await expect(financialVehicle.startSellingSession(asset.address))
+    //     .to.be.revertedWith("Selling session already ended");
+    // });
+
+    it("should revert if called by a non-admin", async () => {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      await expect(financialVehicle.connect(addr1).startSellingSession(asset.address))
+        .to.be.revertedWith("You are not an admin");
+    });
+  });
+
+  describe("endSellingSession()", function () {
+    it("should end the selling session", async function () {
+
+
+      await financialVehicle.startSellingSession(asset.address);
+
+      expect(await financialVehicle.getSellingStatus(asset.address)).to.equal(1); // SellingSessionStarted
+
+      await financialVehicle.endSellingSession(asset.address);
+
+      expect(await financialVehicle.getSellingStatus(asset.address)).to.equal(2); // SellingSessionEnded
+    });
+
+    it("should revert if the selling session has already ended", async function () {
+      
+
+      await financialVehicle.startSellingSession(asset.address);
+
+      expect(await financialVehicle.getSellingStatus(asset.address)).to.equal(1); // SellingSessionStarted
+
+      await financialVehicle.endSellingSession(asset.address);
+
+      expect(await financialVehicle.getSellingStatus(asset.address)).to.equal(2); // SellingSessionEnded
+
+      await expect(financialVehicle.endSellingSession(asset.address)).to.be.revertedWith("Selling session already ended");
+    });
+
+    it("should revert if the selling session has not yet started", async function () {
+      
+
+      await expect(financialVehicle.endSellingSession(asset.address)).to.be.revertedWith("Selling session not started yet");
+    });
+  });
+  
 });
-
-// describe("Financial Vehicle", () => {
-//     const deploySimpleFixture = async () => {
-//         const [owner, otherAccount] = await ethers.getSigners();
-//         const FinancialVehicle = await ethers.getContractFactory("FinancialVehicle");
-//         const financialVehicle = await FinancialVehicle.deploy('0x5FbDB2315678afecb367f032d93F642f64180aa3', admins);
-//         return { financialVehicle, owner, otherAccount };
-//     }
-
-//     describe("Deployment", () => {
-//         it("should return the correct value after the contract deploy", async () => {
-//             const { financialVehicle } = await loadFixture(deploySimpleFixture);
-//         });
-
-//         it("should deploy the contract successfully", async function () {
-//             const { financialVehicle } = await loadFixture(deploySimpleFixture);
-//             expect(await financialVehicle.master()).to.equal(address(0x456));
-//         });
-
-//         it("should set up the roles correctly", async function () {
-//             const { financialVehicle } = await loadFixture(deploySimpleFixture);
-//             expect(await financialVehicle.hasRole(DEFAULT_ADMIN_ROLE, ethers.provider.getSigner(0).getAddress())).to.equal(true);
-//             expect(await financialVehicle.hasRole(ADMIN_ROLE, address(0x123))).to.equal(true);
-//         });
-//     });
-// });
