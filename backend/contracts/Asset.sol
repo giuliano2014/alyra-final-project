dff9fa1c82339940e107360fc4adc0a807cdf697
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-04-08 15:50:14 +0200

    Create Asset have been fixed

--- backend/contracts/Asset.sol
+++ backend/contracts/Asset.sol
@@ -1,50 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-
-contract Asset is ERC20, Initializable {
-
-    string private name_;
-    string private symbol_;
-
-    constructor() ERC20("", "") {
-        _disableInitializers();
-    }
-
-    // @TODO: don't work with internal ???
-    // @TODO: onlyAdmin
-    function initialize(
-        string calldata _name,
-        string calldata _symbol,
-        uint256 _totalSupply
-    ) 
-        public
-        initializer
-    {
-        name_ = _name;
-        symbol_ = _symbol;
-        _mint(msg.sender, _totalSupply);
-    }
-
-    // @TODO: don't work with internal ???
-    // @TODO: onlyAdmin
-    function price(uint256 _amount) public pure virtual returns (uint256) {
-        require(_amount > 100, "Amount must be greater than 100");
-        return _amount * 1 wei;
-    }
-}
-
 // pragma solidity 0.8.19;
 
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-// contract Asset is ERC20Upgradeable {
+// contract Asset is ERC20, Initializable {
 
 //     string private name_;
 //     string private symbol_;
 
+//     constructor() ERC20("", "") {
+//         _disableInitializers();
+//     }
+
+//     // @TODO: don't work with internal ???
+//     // @TODO: onlyAdmin
 //     function initialize(
 //         string calldata _name,
 //         string calldata _symbol,
@@ -53,12 +23,42 @@ contract Asset is ERC20, Initializable {
 //         public
 //         initializer
 //     {
-//         __ERC20_init(_name, _symbol);
+//         name_ = _name;
+//         symbol_ = _symbol;
 //         _mint(msg.sender, _totalSupply);
 //     }
 
+//     // @TODO: don't work with internal ???
+//     // @TODO: onlyAdmin
 //     function price(uint256 _amount) public pure virtual returns (uint256) {
 //         require(_amount > 100, "Amount must be greater than 100");
 //         return _amount * 1 wei;
 //     }
 // }
+
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+contract Asset is ERC20Upgradeable {
+
+    string private name_;
+    string private symbol_;
+
+    function initialize(
+        string calldata _name,
+        string calldata _symbol,
+        uint256 _totalSupply
+    ) 
+        public
+        initializer
+    {
+        __ERC20_init(_name, _symbol);
+        _mint(msg.sender, _totalSupply);
+    }
+
+    function price(uint256 _amount) public pure virtual returns (uint256) {
+        require(_amount > 100, "Amount must be greater than 100");
+        return _amount * 1 wei;
+    }
+}
