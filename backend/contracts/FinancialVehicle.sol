7642aa8c522042e2603287ad0be9cd4e85f0440d
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-04-08 18:31:36 +0200

    Natspec have been added on FinancialVehicle contract

--- backend/contracts/FinancialVehicle.sol
+++ backend/contracts/FinancialVehicle.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./Asset.sol";
 
+/**
+ * @title FinancialVehicle
+ * @dev A contract for managing dynamically assets that are ERC20 tokens and more.
+ */
 contract FinancialVehicle is AccessControl {
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
@@ -37,6 +41,11 @@ contract FinancialVehicle is AccessControl {
     address internal master;
     Token[] private assets;
 
+    /**
+     * @notice Constructor to initialize the contract with a master asset and a list of admin addresses.
+     * @param _master The master asset address.
+     * @param _admins The list of admin addresses.
+     */
     constructor(address _master, address[] memory _admins) {
         master = _master;
 
@@ -68,6 +77,12 @@ contract FinancialVehicle is AccessControl {
     //     return Asset(_assetAddress).transferFrom(address(this), msg.sender, _amount);
     // }
 
+    /**
+     * @notice Allows a user to buy an asset token by sending Ether.
+     * @param _assetAddress The address of the asset token to buy.
+     * @param _amount The amount of tokens to buy.
+     * @return A boolean indicating whether the purchase was successful.
+     */
     function buyToken(address _assetAddress, uint256 _amount) external payable onlyUser returns (bool) {
         uint256 tokenPrice = Asset(_assetAddress).price(_amount);
         require(msg.value == tokenPrice, "Incorrect ether amount");
@@ -75,6 +90,13 @@ contract FinancialVehicle is AccessControl {
         return Asset(_assetAddress).transferFrom(address(this), msg.sender, _amount);
     }
 
+    /**
+     * @notice Allows an admin to create a new asset token.
+     * @param _name The name of the asset token.
+     * @param _symbol The symbol of the asset token.
+     * @param _totalSupply The total supply of the asset token.
+     * @return clone of the newly created asset token.
+     */
     function createAsset(
         string calldata _name,
         string calldata _symbol,
@@ -92,6 +114,10 @@ contract FinancialVehicle is AccessControl {
         return clone;
     }
 
+    /**
+     * @notice Allows an admin to end the current selling session of an asset token.
+     * @param _assetAddress The address of the asset token.
+     */
     function endSellingSession(address _assetAddress) external onlyAdmin {
         require(sellingStatus[_assetAddress] != SellingStatus.SellingSessionEnded, "Selling session already ended");
         require(sellingStatus[_assetAddress] == SellingStatus.SellingSessionStarted, "Selling session not started yet");
@@ -104,10 +130,20 @@ contract FinancialVehicle is AccessControl {
         return assets;
     }
 
+    /**
+     * @notice Allows an admin to get the balance of an asset token.
+     * @param _assetAddress The address of the asset token.
+     * @param _account The address of the account to get the balance of.
+     * @return The balance of the account.
+     */
     function getBalance(address _assetAddress, address _account) external view returns (uint256) {
         return Asset(_assetAddress).balanceOf(_account);
     }
 
+    /**
+     * @notice Allows an admin to get the balance of the financial vehicle.
+     * @return The balance of the financial vehicle.
+     */
     function getBalanceOfFinancialVehicle() external view onlyAdmin returns (uint256) {
         return address(this).balance;
     }
@@ -117,6 +153,10 @@ contract FinancialVehicle is AccessControl {
         return sellingStatus[_assetAddress];
     }
 
+    /**
+     * @notice Allows an admin to start a selling session of an asset token.
+     * @param _assetAddress The address of the asset token.
+     */
     function startSellingSession(address _assetAddress) external onlyAdmin {
         require(sellingStatus[_assetAddress] != SellingStatus.SellingSessionEnded, "Selling session already ended");
         require(sellingStatus[_assetAddress] == SellingStatus.NoCurrentSellingSession, "Selling session already started");
@@ -125,13 +165,18 @@ contract FinancialVehicle is AccessControl {
         emit SellingStatusChange(_assetAddress, SellingStatus.SellingSessionStarted);
     }
 
+    /**
+     * @notice Allows an admin to withdraw Ether from the financial vehicle to a recipient.
+     * @param amount The amount of Ether to withdraw.
+     * @param recipient The address of the recipient.
+     */
     function withdrawFromFinancialVehicle(uint256 amount, address payable recipient) external onlyAdmin {
         require(address(this).balance >= amount, "Insufficient balance");
 
         recipient.transfer(amount);
         emit WithdrawFromFinancialVehicle(recipient, amount);
     }
-
+    
     fallback() external payable {
         emit Received(msg.value);
     }
